ad82933113f4f5e8d34a67d22c5e1d838bbb4348
Sergey Shulyak <sergshulya@gmail.com>
2020-07-08 15:05:20 +0300

    Add webhook

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,4 @@
-// import fs from "fs"
+import fs from "fs"
 import Telegraf, { SceneContextMessageUpdate } from "telegraf"
 
 import logger from "./config/logger"
@@ -13,14 +13,14 @@ import { startCronJobs } from "./cronJobs"
 
 export let bot: Bot
 
-// const tlsOptions = {
-//   key: fs.readFileSync("server-key.pem"),
-//   cert: fs.readFileSync("server-cert.pem"),
-//   ca: [
-//     // This is necessary only if the client uses a self-signed certificate.
-//     fs.readFileSync("ca-cert.pem")
-//   ]
-// }
+const tlsOptions = {
+  key: fs.readFileSync("server-key.pem"),
+  cert: fs.readFileSync("server-cert.pem"),
+  ca: [
+    // This is necessary only if the client uses a self-signed certificate.
+    fs.readFileSync("ca-cert.pem")
+  ]
+}
 
 function createAndConfigureBot(): Bot {
   bot = new Telegraf(env.TELEGRAM_BOT_API_TOKEN)
@@ -46,31 +46,17 @@ export async function start(): Promise<void> {
   const bot = createAndConfigureBot()
   startCronJobs()
 
-  // await bot.telegram.setWebhook(
-  //   `${process.env.URL}/bot-${env.TELEGRAM_BOT_API_TOKEN}`
-  // )
+  if (process.env.NODE_ENV !== "production") {
+    await bot.launch()
+  } else {
+    await bot.telegram.setWebhook(
+      `https://${process.env.HEROKU_APP_NAME}.herokuapp.com/bot-${env.TELEGRAM_BOT_API_TOKEN}`
+    )
 
-  // await bot.telegram.setWebhook(`https://37.57.33.136:8443/bot`, {
-  //   source: "server-cert.pem"
-  // })
-
-  // bot.startWebhook(
-  //   `/bot`,
-  //   tlsOptions,
-  //   8443
-  //   // process.env.PORT as any
-  // )
-
-  // require("http")
-  //   .createServer(bot.webhookCallback(`/bot-${env.TELEGRAM_BOT_API_TOKEN}`))
-  //   .listen(3000)
-
-  // require("https")
-  //   .createServer(
-  //     tlsOptions,
-  //     bot.webhookCallback(`/bot-${env.TELEGRAM_BOT_API_TOKEN}`)
-  //   )
-  //   .listen(8443)
-
-  await bot.launch()
+    bot.startWebhook(
+      `/bot-${env.TELEGRAM_BOT_API_TOKEN}`,
+      tlsOptions,
+      process.env.PORT as any
+    )
+  }
 }
